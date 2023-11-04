f9ce58f016ce7171103f1017958b81be7028e521
Aleksandr Kazak-Kazakevich <sanbz2007@icloud.com>
2023-11-05 03:47:32 +0400

    Basic analysis works

---
+++ Sali/Components/AnalyzerView/AnalyzerDrawing.metal
@@ -0,0 +1,35 @@
+//
+//  AnalyzerDrawing.metal
+//  Sali
+//
+//  Created by Aleksandr Kazak-Kazakevich on 05.11.2023.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+kernel void draw_metering(texture2d<float, access::write> output [[texture(0)]], uint2 gid [[thread_position_in_grid]], constant float *metering [[buffer(0)]], constant int &m_count [[buffer(1)]], constant int &buckets_count [[buffer(2)]]) {
+    float4 color = float4(0.745, 0.204, 0.333, 1.0);
+    float4 still_color = float4(0.0, 0.0, 0.0, 0.0);
+    
+    int bucket_position = floor((float(gid[0]) / output.get_width()) * float(buckets_count));
+    
+    int m_units_by_bucket = m_count / buckets_count;
+    
+    int start_in_units = bucket_position * m_units_by_bucket;
+    
+    float avg = 0.0;
+    int i;
+    
+    for (i = 0; i < m_units_by_bucket; i++) {
+        avg += metering[start_in_units + i];
+    }
+    
+    avg /= i;
+    
+    float n_position_in_drawable = float(gid[1]) / float(output.get_height());
+    
+    bool is_colored = n_position_in_drawable < avg;
+    
+    output.write(is_colored ? color : still_color, gid);
+}
---
+++ Sali/Components/AnalyzerView/AnalyzerView.swift
@@ -0,0 +1,133 @@
+//
+//  AnalyzerView.swift
+//  Sali
+//
+//  Created by Aleksandr Kazak-Kazakevich on 01.11.2023.
+//
+
+import MetalKit
+import UIKit
+
+final class AnalyzerView: UIView {
+    
+    // MARK: Private Properties
+    private let constants = Constants()
+    private let device: MTLDevice
+    private let commandQueue: MTLCommandQueue
+    private let pipelineState: MTLComputePipelineState
+    private var meteringBuffer: MTLBuffer?
+    private var meteringCount: Int?
+    private let replacementBuffer: MTLBuffer
+    private let replacementCount: Int
+    
+    // MARK: Visual Components
+    private lazy var metalView: MTKView = {
+        let view = MTKView(frame: .zero, device: device)
+        view.delegate = self
+        view.framebufferOnly = false
+        
+        return view
+    }()
+    
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        device = MTLCreateSystemDefaultDevice()!
+        commandQueue = device.makeCommandQueue()!
+        let library = device.makeDefaultLibrary()!
+        let function = library.makeFunction(name: "draw_metering")!
+        pipelineState = try! device.makeComputePipelineState(function: function)
+        
+        replacementBuffer = device.makeBuffer(length: MemoryLayout<Float>.size * constants.frequencyBucketsCount)!
+        replacementCount = constants.frequencyBucketsCount
+        
+        super.init(frame: frame)
+        
+        setupView()
+        addSubviews()
+    }
+    
+    convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: UIView
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        metalView.frame = bounds
+    }
+    
+    // MARK: Public Methods
+    func updateMetering(_ metering: [Float]) {
+        meteringBuffer = device.makeBuffer(length: MemoryLayout<Float>.size * metering.count)
+        memcpy(meteringBuffer?.contents(), metering, MemoryLayout<Float>.size * metering.count)
+        meteringCount = metering.count
+    }
+}
+
+// MARK: - MTKViewDelegate
+extension AnalyzerView: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
+        guard let commandEncoder = commandBuffer.makeComputeCommandEncoder() else { return }
+        guard let drawable = view.currentDrawable else { return }
+        
+        var bucketsCount = constants.frequencyBucketsCount
+        commandEncoder.setComputePipelineState(pipelineState)
+        commandEncoder.setTexture(drawable.texture, index: 0)
+        
+        if let meteringBuffer, let meteringCount {
+            commandEncoder.setBuffer(meteringBuffer, offset: 0, index: 0)
+            var count = meteringCount
+            commandEncoder.setBytes(&count, length: MemoryLayout<Int>.stride, index: 1)
+        } else {
+            commandEncoder.setBuffer(replacementBuffer, offset: 0, index: 0)
+            var replacedCount = replacementCount
+            commandEncoder.setBytes(&replacedCount, length: MemoryLayout<Int>.stride, index: 1)
+        }
+        
+        commandEncoder.setBytes(&bucketsCount, length: MemoryLayout<Int>.stride, index: 2)
+        let width = pipelineState.threadExecutionWidth
+        let height = pipelineState.maxTotalThreadsPerThreadgroup / width
+        
+        let threadsPerGroup = MTLSize(width: width, height: height, depth: 1)
+        let threadGroupsPerGrid = MTLSize(width: Int(view.drawableSize.width) / width, height: Int(view.drawableSize.height) / height, depth: 1)
+//        commandEncoder.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerGroup)
+        commandEncoder.dispatchThreadgroups(threadGroupsPerGrid, threadsPerThreadgroup: threadsPerGroup)
+        
+        commandEncoder.endEncoding()
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+        
+        meteringBuffer = nil
+        meteringCount = nil
+    }
+}
+
+// MARK: Private Methods
+extension AnalyzerView {
+    private func setupView() {
+        backgroundColor = .accent
+        layer.cornerRadius = 8
+        layer.cornerCurve = .continuous
+    }
+    
+    private func addSubviews() {
+        addSubview(metalView)
+    }
+}
+
+// MARK: Constants
+extension AnalyzerView {
+    private struct Constants {
+        let frequencyBucketsCount = 32
+    }
+}
